a7208db6f5aa417468801a3e0c637d6190421b85
Geovane <geovanesantos200314@gmail.com>
2026-04-01 10:05:01 -0300

    feature/crear-arquivo-js

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ const operação= 2+2;
 const operação2= 2-2;
 const operação3= 2/2;
 const operação4= 2*2;
-console.log(operação + operação2 + operação3 + operação4)
+console.log(operação + operação2 + operação3 + operação4);
